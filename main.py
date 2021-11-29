e383941368240518ecffa8cca22e8b98cd2da0b5
kouhei <aoikuma1234@gmail.com>
2021-11-29 18:15:05 +0900

    language-test

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from sklearn import svm, metrics
 import glob, os.path, re, json
 
 
-# テキストを読んで出現頻度を調べる --- (※1)
+# テキストを読んで出現頻度を調べる 
 def check_freq(fname):
     name = os.path.basename(fname)
     lang = re.match(r'^[a-z]{2,}', name).group()
     with open(fname, "r", encoding="utf-8") as f:
         text = f.read()
-    text = text.lower() # 小文字に変換
+    text = text.lower() 
     # カウンタを0に
     cnt = [0 for n in range(0, 26)]
     code_a = ord("a")
     code_z = ord("z")
-    # アルファベットの出現回数を調べる --- (※2)
+    # アルファベットの出現回数を調べる 
     for ch in text:
         n = ord(ch)
         if code_a <= n <= code_z: # a-zの間なら
             cnt[n - code_a] += 1
-    # 正規化する --- (※3)
+    # 正規化
     total = sum(cnt)
     freq = list(map(lambda n: n / total, cnt))
     return (freq, lang)
@@ -36,11 +36,11 @@ def load_files(path):
     
 data = load_files("./lang_data/train/*.txt")
 test = load_files("./lang_data/test/*.txt")
-# 今後のためにJSONで結果を保存
+# JSONで結果を保存
 with open("./lang_data/freq.json", "w", encoding="utf-8") as fp:
     json.dump([data, test], fp)
 
-# 学習 --- (※4)
+# 学習 --- 
 clf = svm.SVC()
 clf.fit(data["freqs"], data["labels"])
 
